b3df2d96cbf2cbc1e5efe95267903964d76947cc
jpess <julien.pession@gmail.com>
2022-02-03 01:05:57 +0800

    Init const alphabet array Loop for on array and concatenate in result Display result + enter

--- 001_alphabet.js
+++ 001_alphabet.js
@@ -2,3 +2,10 @@
 /* L'Alphabet */
 
 //Programme qui affiche l’alphabet en lettres minuscules suivi d’un retour à la ligne
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'];
+let result = '';
+for (var i = 0; i < alphabet.length; i++) {
+  result += alphabet[i];
+}
+console.log(result);
+console.log(); //retour a la ligne
