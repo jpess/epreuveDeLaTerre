505aff853fd220f7e747f4ad84cc5f589530105e
jpess <julien.pession@gmail.com>
2022-02-03 09:30:03 +0800

    clean code

--- 001_alphabet.js
+++ 001_alphabet.js
@@ -1,10 +1,19 @@
+/**
+* L'Alphabet
+*/
 
-/* L'Alphabet */
+/*
+* Write a program that displays the alphabet in lowercase with a line break.
+*/
 
-//Programme qui affiche l’alphabet en lettres minuscules suivi d’un retour à la ligne
+// Array of alphabet letters
 const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'];
+
+// For each letter in alphabet array, concatenate the letter to the variable result.
 let result = '';
-for (var i = 0; i < alphabet.length; i++) {
+for (let i = 0; i < alphabet.length; i++) {
   result += alphabet[i];
 }
+
+// Display result wih a line break
 console.log(result + "\n");
