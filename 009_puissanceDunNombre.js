610434add1b068e57c403630d65f9809cf82ee6e
jpess <julien.pession@gmail.com>
2022-02-08 22:48:36 +0800

    prise en charge des erreurs

--- 009_puissanceDunNombre.js
+++ 009_puissanceDunNombre.js
@@ -8,15 +8,21 @@
 const { argv } = require('process');
 
 if( argv.length !== 4){
-  console.log("Veuillez entrer 2 arguments.");
-} else if( isNaN(argv[2] || isNaN(argv[3]))){
-  console.log("Veuillez entrer 2 nombres.");
+  console.log("Veillez entrer 2 arguments.");
+} else if( isNaN(argv[2]) || isNaN(argv[3])){
+    console.log("Veuillez entrer 2 nombres.");
+} else if( argv[3] < 0){
+    console.log("L'exposant ne peut pas etre negatif.");
+} else if( argv[2] < 0){
+    console.log("On va rester sur des nombres positifs pour l'instant ;) ");
+} else if( !Number.isInteger( Number( argv[3] ) ) ){
+    console.log("On va rester sur des expostants entiers aussi :) ");
 } else {
-  let result = 1;
-  for(let i = 0; i < argv[3]; i++){
-    result *= argv[2];
-  }
-  console.log(result);
-  //We can also use Math.pow(argv[2], argv[3]);
-  //console.log(Math.pow(argv[2], argv[3]));
+    let result = 1;
+    for(let i = 0; i < argv[3]; i++){
+      result *= argv[2];
+    }
+    console.log(result);
+    //We can also use Math.pow(argv[2], argv[3]);
+    //console.log(Math.pow(argv[2], argv[3]));
 }
