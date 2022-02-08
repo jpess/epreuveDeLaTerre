ece92c573a124343d7ea771fe354af9de08ee1ca
jpess <julien.pession@gmail.com>
2022-02-08 21:53:14 +0800

    complete

--- 009_puissanceDunNombre.js
+++ 009_puissanceDunNombre.js
@@ -5,3 +5,17 @@
 /*
 * Write a program that displays the result argument 1 to the power argument 2
 */
+const { argv } = require('process');
+
+if( argv.lenght !== 4){
+  console.log("Veuillez entrer 2 arguments.");
+} else if( isNaN(argv[2] || isNaN(argv[3]))){
+  console.log("Veuillez entrer 2 nombres.");
+} else {
+  let result = 1;
+  for(let i = 0; i < argv[3]; i++){
+    result *= argv[2];
+  }
+  console.log(result);
+  //We can also use Math.pow(argv[2], argv[3]);
+}
