147830f585117317ad574e9ca18a15b472125da3
jpess <julien.pession@gmail.com>
2022-02-08 21:44:46 +0800

    init

---
+++ 008_tailleDuneChaine.js
@@ -0,0 +1,20 @@
+/**
+* Taille d'une chaine
+*/
+
+/*
+* Write a program that displays the passed String number of characters
+*/
+
+//Retrieve the arguments
+const { argv } = require('process');
+
+if( argv.length !== 3 ){
+  //Test if exactly ONE argument is passed
+  console.log("erreur.");
+} else if(!isNaN(argv[2])){
+    //Test if argument is not a number
+    console.log("erreur.");
+} else {
+  console.log(argv[2].length);
+}
