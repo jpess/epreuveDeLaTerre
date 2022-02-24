6898117f5a33d9ecb5559e3d66106b8be704d1dd
jpess <julien.pession@gmail.com>
2022-02-24 23:08:34 +0800

    completed

--- 013_12to24.js
+++ 013_12to24.js
@@ -6,15 +6,39 @@
 * Write a program that format a 12-hour time to a 24-hour time
 */
 
-//We will skip argment error handling
-//We suppose that the format is respected : leading 0 for hours before 10 (e.g., 09:05)
+//We will skip argument error handling, and we suppose the time format is respected (no 12:30AM, nor 24:20PM)
 
 const {argv} = require('process');
 
 //Retrieve argv[2] as a String
-const time24h = argv[2];
+const time12h = argv[2];
 
-//Split it to extract hours from minutes
-const time24Split = time24h.split(":");
-let hours24 = time24Split[0];
-const minutes24 = time24Split[1];
+//Split it to extract hours from minutes and period (11:40PM)
+const period = time12h.substr(time12h.length - 2);
+const time12Split = time12h.substr(0, time12h.length - 2).split(":");
+let hours12 = time12Split[0];
+const minutes12 = time12Split[1];
+
+//Check special values
+switch (hours12) {
+  case '0':
+  case '00':
+    hours12 = '00';
+    break;
+  case '12':
+    hours12 = '12';
+    break;
+  default:
+    if(period === "AM"){
+      //hours12 don't need to be formatted
+    } else if(period === "PM"){
+      //Add 12 to hours12, then mod 24
+      hours12 = (Number(hours12) + 12) % 24;
+    } else {
+      console.log("Erreur de format..");
+    }
+}
+
+//Display formatted result
+const result = hours12+":"+minutes12;
+console.log(result);
