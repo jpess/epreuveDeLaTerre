2467d57477415933fd1194e8e4580ed3c6a9d021
jpess <julien.pession@gmail.com>
2022-02-26 22:52:15 +0800

    init

---
+++ 015_trieOuPas.js
@@ -0,0 +1,10 @@
+//il suffit de loop For num[0] until num[lenght] et si jamais num[i] > num[i+1] return 'Pas Triee', sinon Trie
+/**
+* Trié ou pas
+*/
+
+/*
+* Write a program that tells if an integer list is sorted or not
+*/
+
+const {argv} = require('process');
