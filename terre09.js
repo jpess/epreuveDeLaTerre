ab51e5576b5f8eff5184ca89ed3bdba652a08d42
Ju <jpess@users.noreply.github.com>
2022-11-16 23:35:17 +0800

    Update terre09.js

--- terre09.js
+++ terre09.js
@@ -5,18 +5,19 @@
 /*
 * Write a program that displays the square root of an Integer
 */
-const { argv } = require('process');
+//const { argv } = require('process');
+import { argv } from 'node:process';
 
 if( argv.length !== 3){
-  console.log("Veuillez entrer un et un seul argument.");
+  console.log("erreur : Veuillez entrer un et un seul argument.");
 } else if( isNaN( argv[2] ) ){
-  console.log("Veuillez entrer un nombre.");
+  console.log("erreur : Veuillez entrer un nombre.");
 } else {
   const number = Number(argv[2]);
   if( !Number.isInteger( number ) ){
-    console.log("Veuillez entrer un entier.");
+    console.log("erreur : Veuillez entrer un entier.");
   } else if( number < 0 ){
-    console.log("Veuillez entrer un entier positif.");
+    console.log("erreur : Veuillez entrer un entier positif.");
   } else {
     console.log(Math.sqrt(number));
   }
